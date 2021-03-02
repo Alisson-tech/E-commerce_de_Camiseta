e73c29da8642d920a8fa9c02cc1b40bbcae7574a
Alisson-tech <alissonf.45@gmail.com>
2021-03-02 15:56:49 -0300

    Criado os Controllers e views da aplicação, Diagrama do Banco de dados

---
+++ EcommerCamiseta/EcommerCamiseta/Controllers/LojaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EcommerCamiseta.Controllers
+{
+    public class LojaController : Controller
+    {
+        // GET: Loja
+        public ActionResult Lojas()
+        {
+            return View();
+        }
+    }
+}
