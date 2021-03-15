e9b62ee8de741ccab2388e35325b8859441fd89c
zicaboom <victormonteirojesus@gmail.com>
2021-03-15 16:42:08 -0300

    erros do compilador corrigidos

---
+++ Ecommercedecamisa/Ecommercedecamisa/Controllers/PesquisaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EcommerCamiseta.Controllers
+{
+    public class PesquisaController : Controller
+    {
+        // GET: Pesquisa
+        public ActionResult RetProdutos()
+        {
+            return View();
+        }
+    }
+}
